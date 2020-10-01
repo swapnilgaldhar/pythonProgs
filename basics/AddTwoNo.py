06b8a73bba7381fdaae64d8239a03a4ecccba286
Amar Sureshrao Yelane <istevesystem@gmail.com>
2020-10-01 13:08:48 +0530

    Update AddTwoNo.py

--- basics/AddTwoNo.py
+++ basics/AddTwoNo.py
@@ -1,5 +1,3 @@
-i = input("Enter 1 no:")
-j = input("Enter 2 no:")
-k = i + j
-print"Addition is: ",k
-#print (k)
+i = int(input("Enter 1 no:"))
+j = int(input("Enter 2 no:"))
+print"Addition is: ",i+j
